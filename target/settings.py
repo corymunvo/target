991496a0b7bc09742a66724e864ac0bf79cdba77
Cory Norris <cory.norris@munvo.com>
2019-11-06 14:15:40 -0500

    added heroku host

--- target/settings.py
+++ target/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'telesto.clientspectrum.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'target-fashion.herokuapp.com']
 
 
 # Application definition
